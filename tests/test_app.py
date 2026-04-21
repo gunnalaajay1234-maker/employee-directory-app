527dfe67e2642b9b855e5d196860b3c5fad23ad7
gunnalaajay1234-maker <gunnalaajay1234@gmail.com>
2026-04-21 16:28:29 +0000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-import time
 
 def test_add_employee():
-    options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    service = Service("C:\\chromedriver\\chromedriver.exe")
 
-    options.binary_location = "/usr/bin/chromium-browser"
-    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service)
 
-    driver = webdriver.Chrome(service=service, options=options)
+    driver.get("http://localhost:5000")  # or your EC2 IP
 
-    driver.get("http://localhost:5000")  # replace with EC2 IP if needed
-
-    input_box = driver.find_element(By.NAME, "employee")
-    input_box.send_keys("Ajay")
-
-    button = driver.find_element(By.TAG_NAME, "button")
-    button.click()
-
-    time.sleep(2)
+    driver.find_element(By.NAME, "employee").send_keys("Ajay")
+    driver.find_element(By.TAG_NAME, "button").click()
 
     assert "Ajay" in driver.page_source
 
