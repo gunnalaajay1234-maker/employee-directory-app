fb0ea646fbdd552ea90f364030cbb7af930e191f
gunnalaajay1234-maker <gunnalaajay1234@gmail.com>
2026-04-21 22:31:42 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 def test_add_employee():
-    # Setup ChromeDriver correctly
-    service = Service(ChromeDriverManager().install())
+    service = Service("C:\\chromedriver\\chromedriver.exe")
+
     driver = webdriver.Chrome(service=service)
 
-    driver.get("http://localhost:5000")
+    driver.get("http://localhost:5000")  # or your EC2 IP
 
     driver.find_element(By.NAME, "employee").send_keys("Ajay")
     driver.find_element(By.TAG_NAME, "button").click()
